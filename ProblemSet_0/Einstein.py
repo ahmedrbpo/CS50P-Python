b7f0c4b55ad67c9917e93005d5b9e6e8f48ad692
cipher_zero <80566259+ahmedrbpo@users.noreply.github.com>
2024-11-15 13:03:12 +0000

    Fri, Nov 15, 2024, 6:33 PM +05:30

---
+++ ProblemSet_0/Einstein.py
@@ -0,0 +1,14 @@
+# ////////////////////////////////////////
+# //                                    //
+# // Name: AAQIL AHMED KULLDIK MANEGAR  //
+# //                                    //
+# // Assignment: Einstein               //
+# //                                    //
+# ////////////////////////////////////////
+
+m: int = int(input("m: "))
+j: int = m * (300000000*300000000)
+print(f"E: {j}")
+
+# check50 cs50/problems/2022/python/einstein
+# submit50 cs50/problems/2022/python/einstein
