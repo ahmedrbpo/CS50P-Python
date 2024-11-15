07f618a37b90e18233ad8765a3519a2379c352ee
cipher_zero <80566259+ahmedrbpo@users.noreply.github.com>
2024-11-15 14:03:37 +0000

    Fri, Nov 15, 2024, 7:33 PM +05:30

---
+++ ProblemSet_6/Lines_Of_Code/goodbye.py
@@ -0,0 +1,16 @@
+# ////////////////////////////////////////
+# //                                    //
+# // Name: AAQIL AHMED KULLDIK MANEGAR  //
+# //                                    //
+# // Assignment: Lines of Code Test     //
+# //                                    //
+# ////////////////////////////////////////
+
+# This is a test program
+def main():
+    name: str = input("Whats your name? ")
+    print(goodbye(name))
+
+# There should only be an output of 5 lines
+def goodbye() -> str:
+    return f"goodbye"
---
+++ ProblemSet_6/Lines_Of_Code/hello.py
@@ -0,0 +1,17 @@
+# ////////////////////////////////////////
+# //                                    //
+# // Name: AAQIL AHMED KULLDIK MANEGAR  //
+# //                                    //
+# // Assignment: Lines of Code Test     //
+# //                                    //
+# ////////////////////////////////////////
+
+# Another Test Program
+def main():
+    name: str = input("whats your name? ")
+    print(hello(name))
+
+
+# There should only be an output of 5 lines
+def hello(to: str) -> str:
+    return f"hello, {to}"
