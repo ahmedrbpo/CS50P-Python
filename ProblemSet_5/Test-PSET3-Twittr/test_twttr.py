e902924916cd97ba2f3de77ffc6bf0f3f98df22e
cipher_zero <80566259+ahmedrbpo@users.noreply.github.com>
2024-11-15 13:33:36 +0000

    Fri, Nov 15, 2024, 7:03 PM +05:30

---
+++ ProblemSet_5/Test-PSET3-Twittr/test_twttr.py
@@ -0,0 +1,19 @@
+
+# ////////////////////////////////////////
+# //                                    //
+# // Name: AAQIL AHMED KULLDIK MANEGAR  //
+# //                                    //
+# // Assignment: Twttr Test             //
+# //                                    //
+# ////////////////////////////////////////
+from twttr import shorten
+
+# // Test the shorten function
+def test_shorten():
+    assert shorten("testing") == "tstng"
+    assert shorten("this is my tweet!") == "ths s my twt!"
+    assert shorten("UPPERCASE") == "PPRCS"
+    assert shorten("s0meth1ng") == "s0mth1ng"
+
+# check50 cs50/problems/2022/python/tests/twttr
+# check50 cs50/problems/2022/python/tests/twttr
