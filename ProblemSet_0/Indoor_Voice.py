db82186e2c0b3dd5d610b89a913cbc47bdeca859
cipher_zero <80566259+ahmedrbpo@users.noreply.github.com>
2024-11-15 13:04:00 +0000

    Fri, Nov 15, 2024, 6:34 PM +05:30

---
+++ ProblemSet_0/Indoor_Voice.py
@@ -0,0 +1,15 @@
+# ////////////////////////////////////////
+# //                                    //
+# // Name: AAQIL AHMED KULLDIK MANEGAR  //
+# //                                    //
+# // Assignment: Einstein               //
+# //                                    //
+# ////////////////////////////////////////
+import sys
+
+for i in sys.argv[1]:
+    print(i.lower(), end="")
+print()
+
+# check50 cs50/problems/2022/python/indoor
+# submit50 cs50/problems/2022/python/indoor
